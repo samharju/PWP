6d468244b3a1d357f01f3440beab28895adc1d6a
Sami Harju <sami.harju@gmail.com>
2021-04-25 22:38:10 +0300

    More tests to rules api

--- tests/test_rules_api.py
+++ tests/test_rules_api.py
@@ -9,6 +9,7 @@ pytestmark = [pytest.mark.django_db]
 
 
 def test_rule_collection(authenticate, dummy_user):
+    """Rule collection returns all rules"""
     Rule.objects.create(
         name='test1',
         author=dummy_user
@@ -21,3 +22,18 @@ def test_rule_collection(authenticate, dummy_user):
     response = client.get(reverse('rules:rule-list'))
     assert response.status_code == status.HTTP_200_OK
     assert len(response.json()['items']) == 2
+
+
+def test_rule_item(authenticate, create_user, dummy_rule):
+    """Only author has full controls"""
+    controls = ['self', 'collection', 'author', 'delete']
+    client = authenticate()
+    res = client.get(reverse('rules:rule-detail', args=(dummy_rule.pk,)))
+    assert res.status_code == status.HTTP_200_OK
+    data = res.json()
+    assert list(data['@controls'].keys()) == controls
+
+    pate = create_user('pate')
+    client = authenticate(pate)
+    res = client.get(reverse('rules:rule-detail', args=(dummy_rule.pk,)))
+    assert 'delete' not in res.json()['@controls']
